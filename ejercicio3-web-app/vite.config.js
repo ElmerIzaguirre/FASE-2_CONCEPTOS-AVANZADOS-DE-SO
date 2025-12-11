ba4472d6e338cb6190c6fa5e2996e56dcaabb0b1
Elmer Izaguirre <144160446+ElmerIzaguirre@users.noreply.github.com>
2025-12-11 00:21:59 -0600

    Add test configuration to Vite config
    Added test configuration for React testing environment.

--- ejercicio3-web-app/vite.config.js
+++ ejercicio3-web-app/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,        // puedes usar describe, it, expect sin importarlas
+    environment: 'jsdom', // simula un DOM para tests de React
+    setupFiles: './src/setupTests.js', // configuración adicional de testing
+  },
+});
