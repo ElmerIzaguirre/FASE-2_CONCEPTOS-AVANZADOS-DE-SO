cef626b92fae2174dae29a53c48ec92c75226b10
Elmer Izaguirre <144160446+ElmerIzaguirre@users.noreply.github.com>
2025-12-10 23:57:00 -0600

    Refactor main.jsx to use React Router

--- ejercicio3-web-app/src/main.jsx
+++ ejercicio3-web-app/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Home() {
+  return <h1>Home — Bienvenido al Ejercicio 3</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/system" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
